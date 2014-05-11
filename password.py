3a5fdde5eb9ad7ff5d1e70a675c447437902606a
Ibrahim Altaweel <Abe@Abes-MacBook-Pro.local>
2014-05-11 04:08:36 -0700

    version 0.1.1

--- password.py
+++ password.py
@@ -1,6 +1,6 @@
 class password_analyzer:
     def __init__(self):
-        self.frequencies={'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0, '0': 0, '1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0}
+        self.frequencies={}
         self.length_of_word ={}
         
     def parse(self):
@@ -29,20 +29,19 @@ class password_analyzer:
                     #add char to frequences, set its count to 1
                         self.frequencies[c]=1
             return
-     
-            
-
-
-
-            
-            
-        
+              
+#creating an object       
 password_analyzer = password_analyzer()
 
-
-
-
+#console output
+#print the passwords
+print("print the passwords:")
 print(password_analyzer.parse())
+
+#Print length of words
+print("The length of passwords:")
 print(password_analyzer.length_of_word)
+
+#Print frequencies of characters
+print("Frequencies:")
 print(password_analyzer.frequencies)
-        
